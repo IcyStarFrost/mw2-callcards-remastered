41a5759bb849a402f099e7ca15c0f2be0d060ef9
IcyStarFrost <frostlegion10@gmail.com>
2025-06-06 15:43:40 -0500

    WIP image sharing

--- lua/mw2callcards/sv_banner_emblem_handout.lua
+++ lua/mw2callcards/sv_banner_emblem_handout.lua
@@ -48,15 +48,55 @@ hook.Add( "OnEntityCreated", "mw2cc_cosmeticassignment", function( ent )
     end )
 end )
 
+local function ServerHasFile( path )
+    print( "Server has " .. path .. "?", file.Exists( "materials/" .. path, "GAME" ) )
+    return file.Exists( "materials/" .. path, "GAME" )
+end
+
+file.CreateDir( "mw2cc_customimagedata" )
+
+local function RequestCustomImage( ply, path )
+    net.Start( "mw2cc_net_customimage" )
+    net.WriteString( path )
+    net.Send( ply )
+
+    print( "Requesting ", path, " from ", ply )
+
+    local image = ""
+
+    net.Receive( "mw2cc_net_customimage", function( len, ply )
+        local size = net.ReadUInt( 32 )
+        local chunk = net.ReadData( size )
+        local isdone = net.ReadBool()
+
+        print( "Received Chunk", string.NiceSize( #chunk ) )
+
+        image = image .. chunk
+
+        if isdone then
+            print( "Completed retrieval" )
+            file.Write( "mw2cc_customimagedata/test.png", image )
+        end
+    end )
+end
+
 net.Receive( "mw2cc_net_clientsendcosmetics", function( len, ply )
     local banner = net.ReadString()
     local emblem = net.ReadString()
 
     if banner != "" then
         ply.mw2cc_banner = banner
+
+        if !ServerHasFile( banner ) then
+            --RequestCustomImage( ply, banner )
+        end
     end
 
     if emblem != "" then
         ply.mw2cc_emblem = emblem
+
+        if !ServerHasFile( emblem ) then
+            --RequestCustomImage( ply, banner )
+        end
     end
 end )
